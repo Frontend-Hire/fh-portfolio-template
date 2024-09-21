d22c2e7fbaadfdf8d08e8d4c90649a3775ef0013
yaralahruthik <yaralahruthikreddy@gmail.com>
2024-09-21 20:42:42 +0530

    contact page

---
+++ src/site-config.ts
@@ -0,0 +1,23 @@
+type SiteConfig = {
+  contact: {
+    name: string;
+    url: string;
+  }[];
+};
+
+export const SITE_CONFIG: SiteConfig = {
+  contact: [
+    {
+      name: "LinkedIn",
+      url: "https://www.linkedin.com/in/yarala-hruthik-reddy/",
+    },
+    {
+      name: "Twitter",
+      url: "https://twitter.com/thisisyhr",
+    },
+    {
+      name: "GitHub",
+      url: "https://github.com/yaralahruthik",
+    },
+  ],
+};
