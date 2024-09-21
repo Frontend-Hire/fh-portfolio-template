35fedb1b0a131c012b52a41448a1929294742217
yaralahruthik <yaralahruthikreddy@gmail.com>
2024-09-21 21:24:44 +0530

    blog and timeline

--- src/site-config.ts
+++ src/site-config.ts
@@ -1,4 +1,10 @@
 type SiteConfig = {
+  timeline: {
+    name: string;
+    url: string;
+    time: string;
+    description?: string;
+  }[];
   contact: {
     name: string;
     url: string;
@@ -6,6 +12,50 @@ type SiteConfig = {
 };
 
 export const SITE_CONFIG: SiteConfig = {
+  timeline: [
+    {
+      name: "withYHR",
+      url: "https://withyhr.com/",
+      time: "2024-Present",
+      description: "Building web products as a dev studio",
+    },
+    {
+      name: "Frontend Hire",
+      url: "https://frontendhire.com/",
+      time: "2023-Present",
+      description: "Trying to solve frontend developer interviews and hiring",
+    },
+    {
+      name: "Freelancer",
+      url: "https://withyhr.com/",
+      time: "2023",
+      description: "Worked with New Relic, HireHunch, and XRG",
+    },
+    {
+      name: "University of Leeds",
+      url: "https://www.leeds.ac.uk/",
+      time: "2022-2023",
+      description: "Got another computer science degree",
+    },
+    {
+      name: "HireHunch",
+      url: "https://hirehunch.com/",
+      time: "2021-2022",
+      description: "Led core UI development for multiple products",
+    },
+    {
+      name: "Piktorlabs",
+      url: "https://piktorlabs.com/",
+      time: "2021",
+      description: "Started the dev career here",
+    },
+    {
+      name: "BML Munjal University",
+      url: "https://www.bmu.edu.in/",
+      time: "2017-2021",
+      description: "Got a computer science degree",
+    },
+  ],
   contact: [
     {
       name: "LinkedIn",
