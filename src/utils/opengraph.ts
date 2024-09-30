b68d4862a371d90b4ef90843ad1be418b405242e
yaralahruthik <yaralahruthikreddy@gmail.com>
2024-10-01 03:14:01 +0530

    absolute imports

--- src/utils/opengraph.ts
+++ src/utils/opengraph.ts
@@ -1,3 +1,5 @@
+import { readFileSync } from "fs";
+
 type FontFileType = "ttf" | "otf" | "woff";
 
 export interface IFont {
@@ -12,4 +14,36 @@ export interface IPostData {
 
 export interface IPost {
   data: IPostData;
+}
+
+export function Template(post: IPost) {
+  const title = post.data.title;
+  const date = new Intl.DateTimeFormat("en-GB", {
+    day: "numeric",
+    month: "short",
+    year: "numeric",
+  })
+    .format(new Date(post.data.publishDate))
+    .replace(",", "");
+
+  return `
+    <div tw="flex p-8 h-full w-full bg-white flex-col justify-between ">
+        <header tw="flex w-full">
+          <p style="font-size:34px;font-family:RobotoBlack">Hruthik</p>  
+        </header>
+        <main tw="flex grow pb-2 flex-col items-start justify-end" >
+              <p style="font-size:34px;font-family:RobotoMedium">
+                    ${title}
+              </p>
+            <p style="font-size:28px;font-family:RobotoLight">
+                    ${date}
+              </p>
+        </main>
+    </div>
+    `;
+}
+
+export function loadFont({ fontName, fileType }: IFont) {
+  const fontPath = `${process.cwd()}/public/font/${fontName}.${fileType}`;
+  return readFileSync(fontPath);
 }
