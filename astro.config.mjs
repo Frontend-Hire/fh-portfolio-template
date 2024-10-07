4b2dbab491036ef7a249a33e939b37079300b09c
Nitan Jana <67586363+NitanJana@users.noreply.github.com>
2024-10-07 18:10:35 +0530

    feat: add custom pagefind integration (#25)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
+import addPagefind from "./integrations/addPagefind.js";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 
@@ -8,8 +9,8 @@ import sitemap from "@astrojs/sitemap";
 export default defineConfig({
   // TODO: Set your site URL https://docs.astro.build/en/guides/integrations-guide/sitemap/#usage
   // TODO: Get this from the site-config
-  site: 'https://portfolio.frontendhire.com/',
-  integrations: [tailwind(), sitemap()],
+  site: "https://portfolio.frontendhire.com/",
+  integrations: [addPagefind(), tailwind(), sitemap()],
   markdown: {
     shikiConfig: {
       wrap: true,
