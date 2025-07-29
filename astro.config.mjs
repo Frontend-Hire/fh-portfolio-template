85f7f193b11733f7e2f227fad48098a62e54fe73
Nitan Jana <67586363+NitanJana@users.noreply.github.com>
2025-07-29 16:36:51 +0000

    deps: upgrade tailwind to v4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,18 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
-import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   // TODO: Set your site URL https://docs.astro.build/en/guides/integrations-guide/sitemap/#usage
   // TODO: Get this from the site-config
   site: "https://portfolio.frontendhire.com/",
-  integrations: [tailwind({ applyBaseStyles: false }), sitemap()],
+  integrations: [sitemap()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
   markdown: {
     shikiConfig: {
       wrap: true,
